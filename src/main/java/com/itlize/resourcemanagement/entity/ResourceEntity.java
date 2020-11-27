deafc5924141a6258cede9e3ddea15746a9b69dc
Aly ElAzab <elazab.1@osu.edu>
2020-11-27 11:03:26 -0500

    Create ResourceEntity.java

---
+++ src/main/java/com/itlize/resourcemanagement/entity/ResourceEntity.java
@@ -0,0 +1,77 @@
+package com.itlize.resourcemanagement.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class ResourceEntity {
+    @Id
+    @GeneratedValue
+    private int resourceCode;
+
+    @Column(name = "resource_name")
+    private String resourceName;
+
+    @Column(name = "editable")
+    private boolean editable;
+
+    @Column(name = "item_id")
+    private int itemId;
+
+    @Column(name = "create_time")
+    private Date createTime;
+
+    @Column(name = "update_time")
+    private Date updateTime;
+
+    public int getResourceCode() {
+        return resourceCode;
+    }
+
+    public void setResourceCode(int resourceCode) {
+        this.resourceCode = resourceCode;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public boolean isEditable() {
+        return editable;
+    }
+
+    public void setEditable(boolean editable) {
+        this.editable = editable;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
