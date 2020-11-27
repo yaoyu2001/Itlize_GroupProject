2ada086d658d9f4c338538eff1e90d85be6032a5
yaoyu2001 <yongchangyao2001@gmail.com>
2020-11-27 13:41:05 -0500

    v 0.01
    Bug fixed

---
+++ src/main/java/com/itlize/resourcemanagement/entity/Role.java
@@ -0,0 +1,5 @@
+package com.itlize.resourcemanagement.entity;
+
+public enum Role {
+    ADMIN, USER;
+}
--- src/main/java/com/itlize/resourcemanagement/entity/User.java
+++ src/main/java/com/itlize/resourcemanagement/entity/User.java
@@ -1,42 +1,143 @@
 package com.itlize.resourcemanagement.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+
+import com.sun.javafx.geom.transform.Identity;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@Table(name = "resource_management")
-public class User extends BaseEntity {
+@Table (name = "user", uniqueConstraints = {@UniqueConstraint (columnNames = "user_name")})
+public class User {
 
-    @Column(name = "user_name")
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "user_id")
+    private long userId;
+
+    @Column (name = "user_name")
     private String userName;
 
-    @Column(name = "password")
+    @Column (name = "password")
     private String password;
+
+    @Enumerated (EnumType.STRING)
+    @Column (name = "role")
+    private Role role;
+
+    @CreatedDate
+    @Column (updatable = false, nullable = false)
+    private Date timeCreate;
+
+    @LastModifiedDate
+    @Column (nullable = false)
+    private Date lastModified;
+
+    @Column (name = "main_image")
+    private String mainImage;
+
+    @Column (name = "email")
+    private String email;
+
+    @Column (name = "first_name")
+    private String firstname;
+
+    @Column (name = "last_name")
+    private String lastname;
+
+    //Constructors
+    public User() {
+    }
+
+
+    public User( String user_name, String password, String firstName, String lastName ) {
+        this.userName = user_name;
+        this.password = password;
+        this.firstname = firstName;
+        this.lastname = lastName;
+    }
+
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId( long userId ) {
+        this.userId = userId;
+    }
 
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
+    public void setUserName( String userName ) {
         this.userName = userName;
     }
 
-    public String getPassword() {
-        return password;
+
+    public Role getRole() {
+        return role;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setRole( Role role ) {
+        this.role = role;
+    }
+
+
+    public Date getTimeCreate() {
+        return timeCreate;
+    }
+
+    public Date getLastUpdated() {
+        return lastModified;
+    }
+
+    public String getMainImage() {
+        return mainImage;
+    }
+
+    public void setMainImage( String mainImage ) {
+        this.mainImage = mainImage;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail( String email ) {
+        this.email = email;
+    }
+
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname( String firstname ) {
+        this.firstname = firstname;
+    }
+
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname( String lastname ) {
+        this.lastname = lastname;
     }
 
     @Override
     public String toString() {
-        return "User [userName=" + userName + ", password=" + password + "]";
+        return "User{" +
+                "id='" + userId + '\'' +
+                ", user_name='" + userName + '\'' +
+                ", create_time=" + timeCreate +
+                ", update_time=" + lastModified +
+                '}';
     }
 
-//    @OneToOne(mappedBy = )
-//    private
 }
-
-
